38d2ade4b2edab9853d7d40b2a6c28d76b710ddf
Shirajuki <shirajuki00@gmail.com>
2022-12-03 18:46:51 +0100

    Finish challenge week2

--- challs/week2/intermediate/index.js
+++ challs/week2/intermediate/index.js
@@ -3,10 +3,27 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
-import giftlist from './list.json';
 
 const FLAG = process.env.FLAG ?? "Itemize{dummy_flag}";
-const KEY = "#¤&¤%eghjifghj3i4jt03jgr33ggggsdW#T##T%SDSGØÆØOPLUÅØHMGb"
+const KEY = "HoHoHo!!!";
+const giftlist = [
+	{ name: "Susie Dylan", status: "naughty", wishgift: "Princess playdough" },
+	{
+		name: "John Nolan",
+		status: "naughty",
+		wishgift: "Lots of V-Bucks giftcards",
+	},
+	{ name: "Tommy Albert", status: "naughty", wishgift: "Playdough" },
+	{ name: "Ellen Foster", status: "naughty", wishgift: "Makeup kit" },
+	{ name: "Bob Malcolm", status: "nice", wishgift: "Pokemon booster bundle" },
+	{ name: "Mary Mea", status: "nice", wishgift: "Lots of Robux giftcards" },
+	{
+		name: "Jack Krystal",
+		status: "nice",
+		wishgift: "Call of Duty: Modern Warfare II, video game",
+	},
+	{ name: "Haxorman L337", status: "nice", wishgift: FLAG },
+];
 
 const app = express();
 app.use(cors());
@@ -21,48 +38,113 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // Helper functions
+const xor = (string1, string2) => {
+	const buf1 = Buffer.from(string1, "utf8");
+	const buf2 = Buffer.from(string2, "utf8");
+	const res = buf1.map((b, i) => b ^ buf2[i % string2.length]);
+	return res.toString("base64");
+};
+
 const parseSession = (req, res) => {
 	const { session: token } = req.cookies;
-	try {
-		jwt.verify(token, KEY)
-	} catch (error) {
+	if (!token) {
 		res.cookie("session", "", { expires: new Date(0) });
 		return res.redirect("/");
 	}
-	const session = jwt.decode(token)
-    console.log(session)
-	return { name: String(session?.name), status: String(session?.status) };
+	try {
+		const [data, signature] = token.split(".");
+		if (!data) throw new Error("Couldn't parse session data!");
+		if (!signature) throw new Error("Couldn't parse signature data!");
+		const name = atob(data);
+		if (atob(xor(signature, KEY)) === name)
+			throw new Error(
+				"Session signature doesn't match the session data after XOR!"
+			);
+	} catch (error) {
+		if (String(error).includes("InvalidCharacterError"))
+			error = "Couldn't parse the base64 session.";
+		res.cookie("session", "", { expires: new Date(0) });
+		return res.redirect("/?error=" + error);
+	}
+
+	const [data, _signature] = token.split(".");
+	const name = atob(data);
+	const child = giftlist.find((child) => child.name === name);
+	return {
+		name: String(child?.name ?? name),
+		status: String(child?.status ?? "waiting"),
+		wishgift: String(child?.wishgift ?? "Coal"),
+	};
 };
 
-const childEnlisted = (name) => {
-	if (giftlist["good"].concat(giftlist["bad"]).concat(giftlist["waiting"]).includes(name)) return true
-}
+const isChildEnlisted = (name) => {
+	return giftlist.find((child) => child.name === name);
+};
+const shortenName = (child) => {
+	const split = child.name.split(" ");
+	let name = child.name;
+	if (split.length > 1) name = split[0] + " " + split[1].substring(0, 1) + ".";
+	return { ...child, displayName: name };
+};
+const displayGiftList = (giftlist) => {
+	return giftlist.map((child) => {
+		const nchild = shortenName(child);
+		return nchild;
+	});
+};
 
 // Begin routes
 app.get("/", (req, res) => {
 	const { session } = req.cookies;
 	if (session) return res.redirect("/dashboard");
-	res.render("index", { error: undefined });
+	res.render("index", {
+		error: (req.query.error ?? "").toString().replace(/>|</g),
+	});
 });
 
 app.post("/", (req, res) => {
 	const { name } = req.body;
 	if (!name) return res.render("index", { error: "Missing field name" });
-	if (childEnlisted(name)) return res.render("index", { error: "Child allready enlisted" });
-    const token = jwt.sign({ name: name, status: "waiting" }, KEY);
-    res.cookie("session", token);
-    res.redirect("/dashboard");
+
+	const child = isChildEnlisted(name);
+	if (child)
+		return res.render("index", {
+			error: "Child already claimed the gift",
+		});
+
+	const data = String(name);
+	const signature = xor(String(name), KEY);
+	const token = `${btoa(data)}.${signature}`.replaceAll("=", "");
+	res.cookie("session", token);
+	return res.redirect("/dashboard");
 });
 
 app.get("/dashboard", (req, res) => {
 	const user = parseSession(req, res);
-	if (!giftlist[user["status"]].includes(user["name"])) {
-		giftlist[user["status"]].push(user["name"])  // add user to list based on status in cookie
+	if (user.status === "nice") {
+		const nuser = shortenName({
+			...user,
+			message: `For being such a nice child this year, here's your early gift: <code>${user.wishgift}</code>`,
+		});
+		return res.render("dashboard", {
+			giftlist: displayGiftList(giftlist),
+			user: nuser,
+		});
+	} else if (user.status === "naughty") {
+		const nuser = shortenName({
+			...user,
+			message: `No early gift for you.`,
+		});
+		return res.render("dashboard", {
+			giftlist: displayGiftList(giftlist),
+			user: nuser,
+		});
 	}
-	if(giftlist["good"].includes(user["name"])) {
-		giftlist["good"].push(FLAG) // add *GIFT* if user is in good list
-	}
-	res.render("dashboard", { giftlist });
+
+	return res.render("dashboard", {
+		giftlist: displayGiftList(giftlist),
+		user: shortenName(user),
+	});
 });
 
 const port = process.env.PORT || 8080;
